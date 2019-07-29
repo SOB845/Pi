d1e83ad6741c08a4a10db3d3f27cd87793bae379
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-07-29 20:20:02 +0430

    src
    Folder which contains the essential code

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Login from './components/routes/Login';
+import Signup from './components/routes/Signup';
+import Profile from './components/routes/Profile';
+import Publication from './components/routes/Publication';
+
+class App extends Component {
+  render() {
+
+    return (
+      <div className="container">
+
+      <BrowserRouter>
+      <Navbar title = "Navbar"/>  
+
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={Signup}/>
+      <Route path="/profile" component={Profile}/>
+      <Route path="/publications" component={Publication}/>
+      </BrowserRouter>
+
+      </div>
+    );
+  }
+}
+export default App;
