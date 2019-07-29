871e3fbed09a600b8e9e662255a3cd36260439d4
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-07-29 20:30:07 +0430

    Add files via upload

---
+++ src/components/routes/Publication.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class Publications extends Component{
+    render(){
+        return(
+            <h1>Our recent papers</h1>
+        );
+    }
+}
+export default Publications;
