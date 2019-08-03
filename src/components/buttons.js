90cfe41dff0f9b3b829394a994b5bc61d1202247
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-08-03 14:28:43 +0430

    Update buttons.js

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -15,7 +15,7 @@ class Buttons extends Component{
     render(){
         return[
             <AwesomeButton type="primary" size="medium">Explore</AwesomeButton>,
-            <AwesomeButtonSocial cssModule={AwesomeButtonStyles} type="github" size="medium" >GitHub</AwesomeButtonSocial>,
+            <AwesomeButtonSocial cssModule={AwesomeButtonStyles} type="github" size="medium" href='https://github.com/SOB845/Pi'>GitHub</AwesomeButtonSocial>,
             <AwesomeButtonSocial cssModule={AwesomeButtonStyles} type="twitter" size="medium" >Twitter</AwesomeButtonSocial>,
         ]
     }
