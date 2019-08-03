4e4eddb95bf1b48b7bc920a96b8a50fd27041275
Sobhan Haggi <39483396+SOB845@users.noreply.github.com>
2019-08-03 12:03:00 +0430

    Create introtxt.js

---
+++ src/components/introtxt.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Intro extends Component{
+    render(){
+        return[
+            <h1 className='intr1'>Π</h1>,
+            <h2 className='intr2'>A natural Language Processing Platform</h2>,
+        ]
+    }
+}
+export default Intro;
